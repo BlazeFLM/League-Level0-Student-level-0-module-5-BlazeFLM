2b6db9dee4fc4693aed2e6dd5b118ff9c251e39b
student <student@ip-172-31-56-31.us-west-2.compute.internal>
2021-11-09 18:23:07 -0800

    11/9/21 6:22 PM

---
+++ src/_01_algorithms/_1_prime_or_not/Algorithms.java
@@ -0,0 +1,22 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class Algorithms {
+	public static void main(String[] args) {
+		String user = JOptionPane.showInputDialog("Give me a number.");
+		boolean prime = true;
+		int userInt = Integer.parseInt(user);
+		for (int i = 2; i < userInt / 2; i++) {
+			if (userInt % i == 0) {
+				prime = false;
+			}
+
+		}
+			if(prime) {
+				JOptionPane.showMessageDialog(null, "is Prime");
+			} else {
+				JOptionPane.showMessageDialog(null,"Not Prime");
+			}
+	}
+}
