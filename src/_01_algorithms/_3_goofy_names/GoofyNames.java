55dc8634b55df07b785d063c73adce40c739df9b
student <student@ip-172-31-50-188.us-west-2.compute.internal>
2021-11-23 18:24:02 -0800

    6:23 PM 11/23/2021

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -11,29 +11,32 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-			JOptionPane.showInputDialog("What is your name?");
+		String answer = JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-			
+		// Run your program to see that this works.
+		System.out.println(answer.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 7).
-		//    HINT: Use .length() to determine the number of characters in the String.
+		// HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < answer.length(); i++) {
+			// 4. Create a char variable to store the next character of the name
+			// use .charAt()
+			char c = answer.charAt(i);
 
-				// 4. Create a char variable to store the next character of the name
-				//    use .charAt()
-	
-				
-				// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
-			
-			
-				// 6. Even characters should be made uppercase and odd characters made lowercase
-				//    HINT: use Character.toUpperCase() or Character.toLowerCase()
-	
-			
-				// 7. ADD the char to the end of the goofyName String
+			// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
+			if (i % 2 == 0) {
+				c = Character.toUpperCase(c);
+			} else {
+				c = Character.toLowerCase(c);
+			}
+			goofyName += c;
+		}
+		// 6. Even characters should be made uppercase and odd characters made lowercase
+		// HINT: use Character.toUpperCase() or Character.toLowerCase()
 
-		
+		// 7. ADD the char to the end of the goofyName String
+
 		// 8. Use pop-up to show user their Goofy name
+		JOptionPane.showMessageDialog(null, goofyName);
 
 	}
 }
-
