3f56679d3786a3cb46cff3c07434043fd16c9ab9
student <student@ip-172-31-54-130.us-west-2.compute.internal>
2021-12-14 18:25:35 -0800

    6:25 PM 12/14/21

--- src/_02_nested_loops/_3_for_loop_gauntlet/GauntletLoop.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/GauntletLoop.java
@@ -5,15 +5,37 @@ public class GauntletLoop {
 	public static void main(String[] args) {
 		 /*for(int i = 0 ; i < 100; i++) {
 			 System.out.println(i);
-		 }*/
-		 /*for(int i = 100; i > -1; i--) {
+		 }
+		 for(int i = 100; i > -1; i--) {
 			 System.out.println(i);
-		 }*/
-		/*for(int i = 2; i < 101; i++) {
+		 }
+		for(int i = 2; i < 101; i++) {
 			if(i % 2 == 0) {
 			System.out.println(i);
-			}*/
-		for(int i = 1; i < 12;i++){;
+			}
+		for(int i = 1; i < 100;i++){
+			if(i % 2 == 1) {
+			System.out.println(i);
+			}
+		}
+		for(int i = 1; i < 501; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i + " is even");
+			} else {
+				System.out.println(i + " is odd");
+			}
+		}
+		for(int i = 0; i < 778;i++) {
+			if(i % 7 == 0) {
+				System.out.println(i);
+			}
+		}int age = 0;
+		for(int i = 2007; i < 2022; i++) {
+			System.out.println("In "+ i + " you were " + (age));
+			age++;
+		}*/
+		for(int i = 0; i < 2; i++) {
+				
 		}
 	}
 }
